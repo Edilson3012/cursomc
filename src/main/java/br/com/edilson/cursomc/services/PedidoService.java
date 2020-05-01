7e4021afdb767a4104719945e48c2b97a905ed80
Edilson3012 <edilsongotardi@live.com>
2020-05-01 13:01:27 -0300

    Endpoint /pedido/{id} disponibilizado

---
+++ src/main/java/br/com/edilson/cursomc/services/PedidoService.java
@@ -0,0 +1,25 @@
+package br.com.edilson.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.edilson.cursomc.domain.Pedido;
+import br.com.edilson.cursomc.repositories.PedidoRepository;
+import br.com.edilson.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+
+	@Autowired
+	private PedidoRepository repo;
+
+	public Pedido buscar(Integer id) {
+		Optional<Pedido> obj = repo.findById(id);
+
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ",Tipo: " + Pedido.class.getName()));
+	}
+
+}

9b55c98e82c99699b68bf0dfe75ead80ab4dcdbc
Edilson3012 <edilsongotardi@live.com>
2020-05-02 13:45:09 -0300

    Listando todas as categorias com DTO

--- src/main/java/br/com/edilson/cursomc/services/CategoriaService.java
+++ src/main/java/br/com/edilson/cursomc/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package br.com.edilson.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,25 +26,29 @@ public class CategoriaService {
 	}
 
 	public Categoria insert(Categoria obj) {
-		
+
 		obj.setId(null);
-		
+
 		return repo.save(obj);
 	}
-	
+
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repo.save(obj);
 	}
-	
+
 	public void delete(Integer id) {
 		find(id);
 		try {
 			repo.deleteById(id);
-		} catch(DataIntegrityViolationException e){
+		} catch (DataIntegrityViolationException e) {
 			throw new DateIntegrityException("Não é possível excluir uma categoria que possui produto!");
 		}
-		
+
+	}
+
+	public List<Categoria> findAll(){
+		return repo.findAll();
 	}
 	
 }
